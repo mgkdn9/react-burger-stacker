23d1b42ac8cfa40f8b2b8e675d6ead99d517082a
mgkdn9 <mkohlberg95@gmail.com>
2021-12-05 19:14:00 -0600

    functionized App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import React, {Component} from 'react'
+import React from 'react'
 // import ingredients from './Ingredients';
 // import Ingredient from './Ingredient'
 import BurgerStacker from './components/BurgerStacker'
 
-export default class App extends Component {
+const App = () => {
   //DONE BY ME BEFORE CODE ALONG
   // state = {
   //   assembledBurger: []
@@ -27,21 +27,20 @@ export default class App extends Component {
   //     assembledBurger: burgerBuffer
   //   })
   // }
-  render() {
-    return (
-      <div className="App">
-        <BurgerStacker />
-      </div>
-      // <div className="App">
-      //   <h1>Burger Stacker</h1>
-      //   <IngredientList
-      //     ingredients={ingredients}
-      //     onClick={this.addToBurger}
-      //   />
-      //   <BurgerPane
-      //     ingredients={this.state.assembledBurger}
-      //   />
-      // </div>
-    )
-  }
+  return (
+    <div className="App">
+      <BurgerStacker />
+    </div>
+    // <div className="App">
+    //   <h1>Burger Stacker</h1>
+    //   <IngredientList
+    //     ingredients={ingredients}
+    //     onClick={this.addToBurger}
+    //   />
+    //   <BurgerPane
+    //     ingredients={this.state.assembledBurger}
+    //   />
+    // </div>
+  )
 }
+export default App
